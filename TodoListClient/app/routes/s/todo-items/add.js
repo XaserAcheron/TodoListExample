c6713ec63fa23e44474ee5266fa4253c2d8371a8
Xaser Acheron <xaser.88@gmail.com>
2018-11-28 11:46:58 -0600

    partial fix for submitting todo items (set an owner)

--- TodoListClient/app/routes/s/todo-items/add.js
+++ TodoListClient/app/routes/s/todo-items/add.js
@@ -3,7 +3,13 @@ import DataRoute from 'ember-data-route';
 
 export default Ember.Route.extend(DataRoute, {
   model() {
-    return this.store.createRecord('todo-item');
+    const newItem = this.store.createRecord('todo-item');
+
+    // [XA] Default the owner to the first user. This is just a temporary shim
+    // to get this to work; in a real app, get the user ID from the session.
+    const currentUser = this.store.peekAll('application-user').get('firstObject');
+    newItem.set('owner', currentUser);
+    return newItem;
   },
 
   actions: {
